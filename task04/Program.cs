cf481cd92c73b34f5de3c68a5e2efa0ed5b7b2b5
Mikhail Kazachkin <mikhail.kazachkin@yandex.ru>
2023-02-03 18:35:42 +0300

    Second homework is done

---
+++ task04/Program.cs
@@ -0,0 +1,88 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Очистка консоли и инициализация переменных
+Console.Clear();
+// Инициализируем переменные
+double? checkInput;                     // Сюда мы положим введенное "число" пользователя
+int userNumber;                         // Сюда мы положим число пользователя. Int меньше Double, так что скушает 
+int result;                             // В эту переменную мы поместим ответ.
+
+// Попробуем получить число, но может придти null
+checkInput = userNumInput("Введите трехзначное число: ");
+// Проверим число, если считать его не получилось, сгенерируем его в диапазоне от 100 до 999.
+userNumber = getOrGenerate(checkInput, 100, 999);
+
+// Нам нужно трехзначное число. Можно отрицательное, но трехзначное. Оно такое?
+if ((Math.Abs(userNumber) < 100) || (Math.Abs(userNumber) > 999))
+{
+    //Число не удовлетворяет условиям задачи. Прощаемся с пользователем
+    Console.WriteLine("Очень жаль, но введенное число не трехзначное.\nПопробуйте в следующий раз.");
+    Environment.Exit(1);
+}
+
+// Решаем задачу.
+result = Math.Abs(((userNumber / 10) % 10));
+Console.WriteLine($"Вторая цифра введенного числа: {result}");
+
+//------------------------------------
+//----- Пользовательские функции -----
+//------------------------------------
+
+// Функция перевода строки пользователя в число. Может вернуть null
+double? userNumInput(string welcomeText = "Введите строку: ")
+{
+    // Инициализация переменных
+    double resultNum;                   // Вернем в ней ответ
+    bool resultTest;                    // Проверим, возможно ли преобразование типов
+    string? resultString;               // Строка, введенная пользователем
+
+    Console.Write(welcomeText);         // Выводим приглашение на ввод числа
+    resultString = Console.ReadLine();  // Получаем строку
+
+    // Начинаем обработку
+    if ((resultString == null) || (resultString == ""))
+    {
+        return null;                    // Возвращаем null, т. к. мы не получили на вводе число
+    }
+    else
+    {
+        // Вечная проблема с десятичными точками и запятыми. В России меняем точки на запятые
+        resultString = resultString.Replace(".", ",");
+        // Попробуем парсить в double
+        resultTest = double.TryParse(resultString, out resultNum);
+        if (!resultTest)
+        {
+            return null;                // Возвращаем null, т. к. мы не получили на вводе число            
+        }
+        else
+        {
+            return resultNum;           // Возвращаем число
+        }
+    }
+}
+
+// Проверяем введенное пользователем число, если оно введено неверно, то получаем число рандомом от Min до Max
+int getOrGenerate(double? someNum, int generateMin, int generateMax)
+{
+    int theNum;                         // Число! Оно обязательно вернется.
+    // Оно там есть?
+    if (someNum == null)
+    {
+        // Числа нет. Получаем случайное число.
+        Console.WriteLine("Очень жаль, но введенное число не может быть обработано.\nЕсли вы считаете это ошибкой, пожалуйста, обратитесь в техническую поддержку.");
+        theNum = new Random().Next(generateMin, generateMax + 1);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Число {theNum} было сгенерировано автоматически.");
+        Console.ResetColor();
+    }
+    else
+    {
+        // someNum у нас double?, поэтому конвертируем в Int32 и работаем с ним.
+        theNum = Convert.ToInt32(someNum.Value);
+    }
+    return theNum;
+}
---
+++ task06/Program.cs
@@ -0,0 +1,95 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Очистка консоли и инициализация переменных
+Console.Clear();
+// Инициализируем переменные
+double? checkInput;                     // Сюда мы положим введенное "число" пользователя
+int userNumber;                         // Сюда мы положим число пользователя. Int меньше Double, так что скушает 
+
+// Массивы на лекциях были. И раз уж функции используем, то и массивы тоже можно. Но решение без них
+string[] week = new string[7] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+// Попробуем получить число, но может придти null
+checkInput = userNumInput("Введите номер дня недели: ");
+// Проверим число, если считать его не получилось, сгенерируем его в диапазоне от 1 до 7.
+userNumber = getOrGenerate(checkInput, 1, 7);
+if ((userNumber > 7) || (userNumber < 1))
+{
+    // Нет такого дня недели. Сгенерируем число сами.
+    userNumber = getOrGenerate(null, 1, 7);
+}
+
+//Пятница прошла, а понедельник не начался? Weekend!
+if ((userNumber > 5) && (userNumber < 8))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{week[userNumber - 1]} - выходной.");
+    Console.ResetColor();
+}
+else
+{
+    Console.WriteLine($"{week[userNumber - 1]} - рабочий день.");
+}
+
+//------------------------------------
+//----- Пользовательские функции -----
+//------------------------------------
+
+// Функция перевода строки пользователя в число. Может вернуть null
+double? userNumInput(string welcomeText = "Введите строку: ")
+{
+    // Инициализация переменных
+    double resultNum;                   // Вернем в ней ответ
+    bool resultTest;                    // Проверим, возможно ли преобразование типов
+    string? resultString;               // Строка, введенная пользователем
+
+    Console.Write(welcomeText);         // Выводим приглашение на ввод числа
+    resultString = Console.ReadLine();  // Получаем строку
+
+    // Начинаем обработку
+    if ((resultString == null) || (resultString == ""))
+    {
+        return null;                    // Возвращаем null, т. к. мы не получили на вводе число
+    }
+    else
+    {
+        // Вечная проблема с десятичными точками и запятыми. В России меняем точки на запятые
+        resultString = resultString.Replace(".", ",");
+        // Попробуем парсить в double
+        resultTest = double.TryParse(resultString, out resultNum);
+        if (!resultTest)
+        {
+            return null;                // Возвращаем null, т. к. мы не получили на вводе число            
+        }
+        else
+        {
+            return resultNum;           // Возвращаем число
+        }
+    }
+}
+
+// Проверяем введенное пользователем число, если оно введено неверно, то получаем число рандомом от Min до Max
+int getOrGenerate(double? someNum, int generateMin, int generateMax)
+{
+    int theNum;                         // Число! Оно обязательно вернется.
+    // Оно там есть?
+    if (someNum == null)
+    {
+        // Числа нет. Получаем случайное число.
+        Console.WriteLine("Очень жаль, но введенное число не может быть обработано.\nЕсли вы считаете это ошибкой, пожалуйста, обратитесь в техническую поддержку.");
+        theNum = new Random().Next(generateMin, generateMax + 1);
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Число {theNum} было сгенерировано автоматически.");
+        Console.ResetColor();
+    }
+    else
+    {
+        // someNum у нас double?, поэтому конвертируем в Int32 и работаем с ним.
+        theNum = Convert.ToInt32(someNum.Value);
+    }
+    return theNum;
+}
